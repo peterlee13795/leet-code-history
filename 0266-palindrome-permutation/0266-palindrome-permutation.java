e0368dae0d1967634bfe7b9b6c60d0cc66c00149
peterlee <lgw888@naver.com>
2024-11-04 15:10:05 +0900

    Time: 0 ms (100.00%), Space: 41.1 MB (83.54%) - LeetHub

--- 0266-palindrome-permutation/0266-palindrome-permutation.java
+++ 0266-palindrome-permutation/0266-palindrome-permutation.java
@@ -3,16 +3,17 @@ class Solution {
     char[] characters = s.toCharArray();
     int[] characterCount = new int[128];
     for (char character : characters) {
-      characterCount[character]++;
+      characterCount[character]++; // character 개수 증분
     }
-    int len = characters.length;
-    boolean isOdd = len % 2 == 1;
-    int oddCount = 0;
+
+    int oddCount = 0; // character 홀수 개수
     for (int i = 0; i < 128; i++) {
       if (characterCount[i] % 2 == 1) {
         oddCount++;
       }
     }
+
+    boolean isOdd = characters.length % 2 == 1; // 문자 홀수 여부
     return isOdd ? oddCount == 1 : oddCount == 0;
   }
 }
