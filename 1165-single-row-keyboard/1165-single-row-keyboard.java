b8a472056b9fd01f3dab3053334d261a9021f6ba
peterlee <lgw888@naver.com>
2024-11-04 15:27:21 +0900

    Time: 4 ms (100.00%), Space: 42.1 MB (69.46%) - LeetHub

--- 1165-single-row-keyboard/1165-single-row-keyboard.java
+++ 1165-single-row-keyboard/1165-single-row-keyboard.java
@@ -1,19 +1,19 @@
 class Solution {
     public int calculateTime(String keyboard, String word) {
-    char[] characters = keyboard.toCharArray();
+    char[] characters = keyboard.toCharArray(); // 키보드 배열
     int len = characters.length;
-    int[] characterIndex = new int[128];
+    int[] characterIndex = new int[128]; // 문자 인덱스
 
     for (int i = 0; i < len; i++) {
-      characterIndex[characters[i]] = i;
+      characterIndex[characters[i]] = i; // 문자 <-> 인덱스 값
     }
-    char[] requiredCharacters = word.toCharArray();
-    int moves = 0;
-    int lastIndex = 0;
+    char[] requiredCharacters = word.toCharArray(); // word 배열
+    int moves = 0; // 이동 값
+    int lastIndex = 0; // 마지막 이동 인덱스
     for (int i = 0, wordLen = requiredCharacters.length; i < wordLen; i++) {
-      int requiredIndex = characterIndex[requiredCharacters[i]];
-      moves += Math.abs(requiredIndex - lastIndex);
-      lastIndex = requiredIndex;
+      int requiredIndex = characterIndex[requiredCharacters[i]]; // word character index
+      moves += Math.abs(requiredIndex - lastIndex); // 현재 이동 경로
+      lastIndex = requiredIndex; 
     }
 
     return moves;
