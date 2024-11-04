0f8c06e1306868c54f2f754bae1855d3e1784339
peterlee <lgw888@naver.com>
2024-11-04 14:21:17 +0900

    Time: 3 ms (50.00%), Space: 43 MB (25.40%) - LeetHub

--- 0734-sentence-similarity/0734-sentence-similarity.java
+++ 0734-sentence-similarity/0734-sentence-similarity.java
@@ -1,12 +1,15 @@
 class Solution {
     public boolean areSentencesSimilar(String[] sentence1, String[] sentence2, List<List<String>> similarPairs) {
+    // 길이가 다르면 false
     if (sentence1.length != sentence2.length) {
       return false;
     }
 
+    // simular mapper 정의 (simularPair의 키-값 2쌍)
     Map<String, Set<String>> mapper1 = new HashMap<>();
     Map<String, Set<String>> mapper2 = new HashMap<>();
 
+    // simuar key-value 정리
     for (List<String> similarPair : similarPairs) {
       String value1 = similarPair.get(0);
       String value2 = similarPair.get(1);
@@ -19,13 +22,23 @@ class Solution {
     for (int i = 0, len = sentence1.length; i < len; i++) {
       String word1 = sentence1[i];
       String word2 = sentence2[i];
-      if (word1.equals(word2)) continue;
+      if (word1.equals(word2)) {
+        continue; // 동일 여부 체크
+      }
+
+      // 양방향 체크
       Set<String> set1 = mapper1.get(word1);
       Set<String> set2 = mapper2.get(word1);
-      if (set1 != null && set1.contains(word2)) continue;
-      if (set2 != null && set2.contains(word2)) continue;
-      return false;
+      if (set1 != null && set1.contains(word2)) {
+        continue;
+      }
+      if (set2 != null && set2.contains(word2)) {
+        continue;
+      }
+
+      return false; // simular 가 아님
     }
-    return true;
+
+    return true; // 체크 완료되어서 simular sentences
   }
 }
