8d823c403efc717525c9624a1105ce3b2dadd8df
peterlee <lgw888@naver.com>
2024-11-29 08:06:02 +0900

    Time: 7 ms (47.35%), Space: 45.5 MB (13.56%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -7,25 +7,28 @@ class Solution {
         
         for(int item: asteroids) {
             // if negative
-            if(item > 0) positives.add(item);
-            else {
-                boolean isExplode = false;
-                while(!positives.isEmpty()) {
-                    int result = positives.peek() + item;
-                    if(result > 0) {
-                        isExplode = true;
-                    } else if (result == 0) {
-                        positives.pop();
-                        isExplode = true;
-                    } else {
-                        positives.pop();
-                    }
-                    if(isExplode) break;
-                }
-                if(positives.isEmpty() && !isExplode) {
-                    remains.add(item);
-                }
+            if(item > 0) {
+                positives.add(item);
+                continue;
             }
+            
+            boolean isExplode = false;
+            while(!positives.isEmpty()) {
+                int result = positives.peek() + item;
+                if(result > 0) {
+                    isExplode = true;
+                } else if (result == 0) {
+                    positives.pop();
+                    isExplode = true;
+                } else {
+                    positives.pop();
+                }
+                if(isExplode) break;
+            }
+            if(positives.isEmpty() && !isExplode) {
+                remains.add(item);
+            }
+            
         }
         
         remains.addAll(positives);
