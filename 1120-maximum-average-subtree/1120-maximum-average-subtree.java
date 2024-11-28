1ecddf742c79ef1d8c70e18173c9f611ef929195
peterlee <lgw888@naver.com>
2024-11-28 15:41:48 +0900

    Time: 0 ms (100.00%), Space: 45.4 MB (21.93%) - LeetHub

--- 1120-maximum-average-subtree/1120-maximum-average-subtree.java
+++ 1120-maximum-average-subtree/1120-maximum-average-subtree.java
@@ -16,19 +16,15 @@
 class Solution {
     double max = 0;
     
-    // 필요한거 
-    // 1. sub tree 노드 개수
-    // 2. sub tree 개수 합산
-    // 연산방법
-    // dfs로 호출
-    // dfs로 왼쪽/오른쪽 순회와 현재까지 쌓인 값 차감 후 계산
     public double maximumAverageSubtree(TreeNode root) {
         dfs(root, new AtomicInteger(), new AtomicInteger());
         return max;
     }
     
     public void dfs(TreeNode node, AtomicInteger previousSum, AtomicInteger previousCount) {
+        // sub tree 개수 합산
         int previousSum0 = previousSum.get();
+        // sub tree 노드 개수
         int previousCount0 = previousCount.get();
         
         if(node.left != null) {
@@ -40,6 +36,7 @@ class Solution {
         int nowSum = previousSum.get() + node.val;
         int nowCount = previousCount.get() + 1;
         
+        // dfs로 왼쪽/오른쪽 순회와 현재까지 쌓인 값 차감 후 계산    
         double currentAverage = (nowSum - previousSum0) / (double)(nowCount - previousCount0);
         max = Math.max(max, currentAverage);
         
