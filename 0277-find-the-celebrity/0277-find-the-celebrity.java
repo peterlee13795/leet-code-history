9285002dafb382a415839a230fab919861db066e
이가원 <lgw888@naver.com>
2025-04-08 08:57:08 +0900

    Time: 492 ms (5.65%), Space: 45.2 MB (93.09%) - LeetHub

--- 0277-find-the-celebrity/0277-find-the-celebrity.java
+++ 0277-find-the-celebrity/0277-find-the-celebrity.java
@@ -3,21 +3,30 @@
 
 public class Solution extends Relation {
     public int findCelebrity(int n) {
-        // list 반복하면서 counts[] == n인거 출력
-        for (int i = 0; i < n; i++) {
-            boolean found = true;
-            for (int j = 0; j < n; j++) {
-                if (i == j) {
-                    continue;
-                }
-                if (!knows(j, i) || knows(i, j)) {
-                    found = false;
-                    break;
-                }
-            }
-            if (found) return i;
-        }
+    // init (나를 알고있음-개수: int[], 내가 다른 사람을 1명이라도 알고있음: boolean[])
+    int[] otherKnowsMeCount = new int[n];
+    boolean[] knowAnyone = new boolean[n];
 
-        return -1;
+    // 1~n  2중 반복
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        boolean know = knows(i, j); // i가 j를 알고 있음
+        if (know && i != j) {
+          otherKnowsMeCount[j]++;
+          knowAnyone[i] = true;
+        }
+      }
     }
+
+    // n 반복
+    for(int i= 0 ; i < n ; i++) {
+      boolean isCelebrity = otherKnowsMeCount[i] == n - 1 && !knowAnyone[i];
+      if(isCelebrity) {
+        return i;
+      }
+    }
+
+    // return -1
+    return -1;
+  }
 }
