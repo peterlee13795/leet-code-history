0b96ba9762f5b5266d7cbac8b9518f58565f633c
이가원 <lgw888@naver.com>
2025-04-09 09:33:53 +0900

    Time: 22 ms (91.16%), Space: 51.5 MB (71.49%) - LeetHub

--- 0582-kill-process/0582-kill-process.java
+++ 0582-kill-process/0582-kill-process.java
@@ -1,26 +1,31 @@
 class Solution {
-   public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
-        // map<Integer, List<Intger>> tree; {key=pid,value=cpid}
-        Map<Integer, List<Integer>> tree = new HashMap<>();
-        int len = ppid.size();
-        for(int i= 0 ;i < len; i++) {
-            tree.computeIfAbsent(ppid.get(i), (key) -> new ArrayList<>()).add(pid.get(i));
-        }
+    public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
+    // init (len, 인접리스트, 답-리스트)
+    int len = pid.size();
+    Map<Integer, List<Integer>> adjusts = new HashMap<>();
+    List<Integer> answer = new ArrayList<>();
 
-        List<Integer> answer = new ArrayList<>();
-        answer.add(kill);
-
-        // tree kill and follow until not found
-        List<Integer> empty = new ArrayList<>();  
-        
-        Queue<Integer> queue = new LinkedList<>(tree.getOrDefault(kill, empty));
-        while (!queue.isEmpty()) {
-            Integer next = queue.poll();
-            answer.add(next);
-            queue.addAll(tree.getOrDefault(next, empty));
-        }
-        return answer;
+    // 부모-자식 인접리스트 charge
+    for(int i =0 ; i < len ; i ++) {
+      int ppidVal = ppid.get(i);
+      if(ppidVal == 0) continue;
+      adjusts.computeIfAbsent(ppidVal, k -> new ArrayList<>()).add(pid.get(i));
     }
-    
-    
+
+    // 큐 반복
+    Queue<Integer> queue = new LinkedList<>();
+    queue.add(kill);
+
+    // 정리 대상 추가
+    while (!queue.isEmpty()) {
+      int ppidVal = queue.poll();
+      answer.add(ppidVal);
+      if(adjusts.containsKey(ppidVal)) {
+        queue.addAll(adjusts.get(ppidVal));  
+      }
+    }
+
+    // return
+    return answer;
+  }
 }
