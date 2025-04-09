5a11f5198080a60b96c4accea4f315d9923ccb97
이가원 <lgw888@naver.com>
2025-04-09 19:26:22 +0900

    Time: 1 ms (34.52%), Space: 41.1 MB (10.71%) - LeetHub

--- 0651-4-keys-keyboard/0651-4-keys-keyboard.java
+++ 0651-4-keys-keyboard/0651-4-keys-keyboard.java
@@ -1,15 +1,22 @@
 class Solution {
     public int maxA(int n) {
-        // dp[n] = math.max(dp[n-1]+1, dp[j] * (i-j-1))
+    // init dp[n]
+    if(n == 1) return 1;
+    int[] dp = new int[n + 1];
+    dp[1] = 1;
+    dp[2] = 2;
 
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        for(int i = 2 ; i <= n; i++) {
-            dp[i] = dp[i-1] + 1;
-            for(int j = 1; j < i; j++) {
-                dp[i] = Math.max(dp[i], dp[j] * (i-j-1));
-            }
-        }
-        return dp[n];
+    // iterate 3~n
+    for(int i = 3; i <= n; i++) {
+      dp[i] = dp[i - 1] + 1;
+      // iterate 1~n-2
+      for(int j = 1; j <= i - 3; j++) {
+        // dp[i] = max(dp[i], dp[j] * (n - j))
+        dp[i] = Math.max(dp[i], dp[j] * (i - j - 1));
+      }
     }
+    // return dp[n]
+    return dp[n];
+  }
+
 }
