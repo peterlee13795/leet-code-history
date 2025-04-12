ffc51749caa65f03116b6b3cde13598843ecf509
이가원 <lgw888@naver.com>
2025-04-12 12:23:15 +0900

    Time: 1 ms (100%), Space: 43.7 MB (88.31%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,30 +1,17 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // init (characterCountMap, longest)
-        Map<Character, Integer> characterCountMap = new HashMap<>();
-        int longest = 0;
+    int[] index = new int[128]; // ASCII 문자 기준
+    int maxLength = 0;
+    int left = 0;
 
-        // two-point iterate
-        for(int lidx = 0, ridx = 0, len = s.length(); ridx < len; ridx++) {
-            // char
-            char c0 = s.charAt(ridx);
-
-            // check char if exist in map
-            while(characterCountMap.containsKey(c0)) {
-                // remove from left until char found from lindex
-                char c1 = s.charAt(lidx++);
-                int c1Count = characterCountMap.get(c1);
-                if(c1Count == 1) {
-                    characterCountMap.remove(c1);
-                } else {
-                    characterCountMap.put(c1, c1Count - 1);
-                }
-            }
-            characterCountMap.put(c0, characterCountMap.getOrDefault(c0, 0) + 1);
-
-            // get longest (rindex - lindex + 1)
-            longest = Math.max(longest, ridx - lidx + 1);
-        }
-        return longest;
+    for (int right = 0; right < s.length(); right++) {
+        char c = s.charAt(right);
+        // 중복 문자가 있으면 left 갱신
+        left = Math.max(index[c], left);
+        maxLength = Math.max(maxLength, right - left + 1);
+        index[c] = right + 1; // 인덱스를 1-based로 저장
     }
+
+    return maxLength;
+}
 }
