4fdc59f39482cf6fbb1067a937bd8e7c16b1e12a
이가원 <lgw888@naver.com>
2025-04-16 09:02:04 +0900

    Time: 2 ms (78.36%), Space: 44.7 MB (50.11%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,31 +1,28 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        // column sets Set[]
-        Set<Character>[] columns = new HashSet[9];
-        for(int i =0 ; i < 9 ; i++) columns[i] = new HashSet<>();
-
-        // grid sets Set[][]
-        Set<Character>[][] grids = new HashSet[3][3];
-        for(int i =0 ; i < 3 ; i++) {
-            for (int j = 0; j < 3; j++) {
-                grids[i][j] = new HashSet<>();
-            }
-        }
-
-        // iterate row & cell
-        for(int i =0 ; i < 9 ; i++) {
-            Set<Character> row = new HashSet<>();
-            char[] brow = board[i];
-            for (int j = 0; j < 9; j++) {
-                char t0 = brow[j];
-                if(t0 == '.') continue;
-                // check if duplicated
-                if(!row.add(t0)) return false;
-                if(!columns[j].add(t0)) return false;
-                if(!grids[i / 3][j / 3].add(t0)) return false;
-            }
-        }
-
-        return true;
+    // init xAxis count, yAxis count, square count Set<Integer>[]
+    Set<Integer>[] xAxis = new HashSet[9];
+    Set<Integer>[] yAxis = new HashSet[9];
+    Set<Integer>[][] subSquare = new HashSet[3][3];
+    for(int i =0 ; i < 9; i++) {
+      xAxis[i] = new HashSet<>();
+      yAxis[i] = new HashSet<>();
+      subSquare[i / 3][i % 3] = new HashSet<>();
     }
+    
+    // iterate xrow and yrow
+    for(int y = 0 ; y < 9; y++) {
+      for(int x = 0 ; x < 9; x++) {
+        // check three axis (x, y, subSquare if contains)
+        if(board[y][x] == '.') continue;
+        int value = board[y][x] - '0';
+        if(!xAxis[x].add(value)) return false;
+        if(!yAxis[y].add(value)) return false;
+        if(!subSquare[y / 3][x / 3].add(value)) return false;
+      }
+    }
+    
+    // return false if not found any
+    return true;
+  }
 }
