066b37a780e2968cd0f11c2e9c3893716c334d91
이가원 <lgw888@naver.com>
2025-04-19 09:43:09 +0900

    Time: 15 ms (87.18%), Space: 43.7 MB (90.31%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+    Arrays.sort(nums);
+    int len = nums.length;
+    List<List<Integer>> answer = new ArrayList<>();
+
+    for (int i = 0; i < len - 3; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+        for (int j = i + 1; j < len - 2; j++) {
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+            int left = j + 1;
+            int right = len - 1;
+
+            while (left < right) {
+                long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    answer.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                }
+            }
+        }
+    }
+
+    return answer;
+}
+
+}
