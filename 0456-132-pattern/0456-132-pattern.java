daf01d4793585228510355914902a27d0a711243
이가원 <lgw888@naver.com>
2025-04-30 23:13:51 +0900

    Time: 40 ms (66.64%), Space: 75.7 MB (54.3%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -1,21 +1,24 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
-        int n = nums.length;
-        Stack<Integer> stack = new Stack<>(); // nums[j] 후보들 저장
-        int second = Integer.MIN_VALUE;       // nums[k] 역할 ('2'에 해당)
+        // second variable
+        int second = Integer.MIN_VALUE;
 
-        // 뒤에서부터 스캔
-        for (int i = n - 1; i >= 0; i--) {
-            if (nums[i] < second) {
-                return true; // 132 패턴 성립
-            }
-            while (!stack.isEmpty() && nums[i] > stack.peek()) {
-                // nums[i]보다 작은 값들을 꺼내서 second 갱신
-                second = stack.pop();
-            }
-            stack.push(nums[i]); // 현재 값은 nums[j] 후보로 스택에 추가
+        // stack
+        Stack<Integer> stack = new Stack<>();
+
+        // iterate num backward
+        for(int i = nums.length - 1; i >= 0 ; i--) {
+            // if current value is smaller than second => returne true
+            if(nums[i] < second) return true;
+
+            // if current value is larger than stack pop => set second variable, to ensure that nums[j] > nums[k]
+            while(!stack.isEmpty() && nums[i] > stack.peek()) second = stack.pop();
+
+            // put current value into stack
+            stack.add(nums[i]);
         }
 
+        // return false
         return false;
     }
 }
