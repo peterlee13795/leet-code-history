427ee1bac86d66279ed6ca6c0f184663e0460e8f
이가원 <lgw888@naver.com>
2025-05-06 01:02:51 +0900

    Time: 2 ms (97.39%), Space: 47.4 MB (84.68%) - LeetHub

--- 0852-friends-of-appropriate-ages/0852-friends-of-appropriate-ages.java
+++ 0852-friends-of-appropriate-ages/0852-friends-of-appropriate-ages.java
@@ -1,24 +1,23 @@
 public class Solution {
     public int numFriendRequests(int[] ages) {
-        int[] count = new int[121]; // 나이 범위 1~120
+        int[] counts = new int[121];
 
-        for (int age : ages) count[age]++;
+        for (int age : ages) {
+            counts[age]++;
+        }
 
         int total = 0;
+        for (int x = 1; x <= 120; x++) {
+            if (counts[x] == 0) continue;
 
-        for (int ageA = 15; ageA <= 120; ageA++) {
-            if (count[ageA] == 0) continue;
+            // 정확히 조건 "age[B] > 0.5 * age[A] + 7"
+            for (int y = (int)(x * 0.5 + 7) + 1; y <= x; y++) {
+                if (counts[y] == 0) continue;
 
-            for (int ageB = 15; ageB <= 120; ageB++) {
-                if (count[ageB] == 0) continue;
-
-                if (ageB <= ageA * 0.5 + 7) continue;
-                if (ageB > ageA) continue;
-
-                if (ageA == ageB) {
-                    total += count[ageA] * (count[ageA] - 1); // 자기 자신 제외
+                if (x == y) {
+                    total += counts[x] * (counts[x] - 1); // 자기 자신 제외
                 } else {
-                    total += count[ageA] * count[ageB];
+                    total += counts[x] * counts[y];
                 }
             }
         }
