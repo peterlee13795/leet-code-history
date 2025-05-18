dd7352738463dec41df89a9fefde10d97f163b33
이가원 <lgw888@naver.com>
2025-05-18 11:50:26 +0900

    Time: 2 ms (82.99%), Space: 44.9 MB (31.64%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,42 @@
+class Solution {
+    List<List<Integer>> answer;
+    int size;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        // init
+        answer = new ArrayList<>();
+        size = candidates.length;
+
+        // sort
+        Arrays.sort(candidates);
+
+        // backtrack
+        backtrack(candidates, target, new ArrayList<>(), 0, 0);
+
+        // return list
+        return answer;
+    }
+
+    private void backtrack(int[] candidates, int target, List<Integer> nodes, int sum, int start) {
+        // iterate candidates
+        for(int i = start; i < size; i++) {
+            int candidate = candidates[i];
+            // add new num to sum
+            int currentSum = sum + candidate;
+
+            // if sum is larger than target => continue
+            if(currentSum > target) return;
+            else if (currentSum < target) {
+                // // if smaller than sum => add to nodes and next backtrack
+                nodes.add(candidate);
+                backtrack(candidates, target, nodes, currentSum, i);
+            } else {
+                // if equals to target => add to answer and return
+                nodes.add(candidate);
+                answer.add(new ArrayList<>(nodes));
+            }
+
+            // // remove last node
+            nodes.remove(nodes.size() - 1);
+        }
+    }
+}
