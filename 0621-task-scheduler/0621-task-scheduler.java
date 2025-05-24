bff22c9ef1494c346b79f3d2f4f609fb879b5a31
이가원 <lgw888@naver.com>
2025-05-24 20:42:39 +0900

    Time: 2 ms (98.74%), Space: 45.9 MB (55.4%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -6,10 +6,9 @@ class Solution {
 
         // 가장 다양하게 나온 종류 확인
         Arrays.sort(frequents);
-        int maxFrequent = frequents[25];
         int maxCount = 1;
         for(int i = 24; i >= 0 ;i--) {
-            if(maxFrequent != frequents[i]) break;
+            if(frequents[25] != frequents[i]) break;
             maxCount++;
         }
 
@@ -17,7 +16,7 @@ class Solution {
         // 1회반복 = n + 1
 
         // 결과값 = 반복수 * 1회반복 + 최대수
-        int emptySlots = (maxFrequent -1) * (n + 1) + maxCount;
+        int emptySlots = (frequents[25] - 1) * (n + 1) + maxCount;
 
         // return Math.max(결과값, length);
         return Math.max(emptySlots, tasks.length);
