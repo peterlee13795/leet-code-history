937b05d5d75467413ad5c3ab9142dcf21d0e8ca6
이가원 <lgw888@naver.com>
2025-08-05 00:28:52 +0900

    Time: 5 ms (75.89%), Space: 58.2 MB (31.68%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,5 +1,17 @@
 class Solution {
     public int maxArea(int[] height) {
-        
+        int max = 0;
+    int lIndex = 0;
+    int rIndex = height.length - 1;
+    
+    while(lIndex < rIndex) {
+      int l = rIndex - lIndex;
+      int h = Math.min(height[rIndex], height[lIndex]);
+      max = Math.max(max, l * h);
+      if(height[rIndex] > height[lIndex]) lIndex++;
+      else rIndex--;
+    }
+    
+    return max;
     }
 }
