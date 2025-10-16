2af3a082adaea6f055e89816752d172fbecd3f45
이가원 <lgw888@naver.com>
2025-10-17 00:51:15 +0900

    Time: 2 ms (99%), Space: 45 MB (66.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,17 +1,21 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // sort n * logn
-        // Arrays.sort(nums);
-
-        // two-pointer n^2
-        int len = nums.length;
-        for(int i = 0; i < len - 1; i++) {
-            for(int j = i + 1; j < len; j++) {
-                if(nums[i] + nums[j] == target) return new int [] {i, j};
+        // define map (key=num, val=index)
+        Map<Integer, Integer> map = new HashMap<>(); 
+        
+        // iterate nums
+        for(int i = 0,len = nums.length; i < len; i++) {
+            // check if left over (target - nums[i]) exists in map
+            int leftover = target - nums[i];
+            // if exist => return
+            if(map.containsKey(leftover)) {
+                return new int[] {map.get(leftover), i };
             }
+            
+            // put nums[i] to map    
+            map.put(nums[i], i);
+            
         }
         return null;
-
-
     }
 }
