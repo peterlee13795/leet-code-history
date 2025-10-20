591eef98421f548c63378a12f8a09c73934d53ad
이가원 <lgw888@naver.com>
2025-10-21 02:14:43 +0900

    Time: 4 ms (85.15%), Space: 45.5 MB (32.83%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,36 +1,39 @@
 class Solution {
+    int len = 0;
     public String convert(String s, int numRows) {
-    if(numRows == 1) {
-      return s;
+        // 예외처리 (numRows == 1 || s.length() <= numRows)
+        if(numRows  == 1 || s.length() <= numRows) {
+            return s;
+        }
+
+        // StringBuilder 각 라인 별 배열 생성
+        StringBuilder[] sb = new StringBuilder[numRows];
+        for(int i =0 ; i < numRows; i++) {
+            sb[i] = new StringBuilder();
+        }
+
+        boolean goingUp = false;
+        int currentRow = 0;
+        int len = s.length();
+
+        // s 문자열 반복문
+        for(char c: s.toCharArray()) {
+            // 현재 행 위치 찾아서 append
+            sb[currentRow].append(c);
+
+            // up 이나 down에 따라 행 위치 변경
+            if(currentRow == 0) goingUp = false;
+            else if (currentRow == numRows - 1) goingUp = true;
+            currentRow += (goingUp ? -1 : 1);
+        }
+
+        StringBuilder concat = new StringBuilder();
+        for(int i =0 ; i < numRows; i++) {
+            concat.append(sb[i].toString());
+        }
+
+        // return 
+        return concat.toString();
+        
     }
-    // len
-    int len = s.length();
-
-    // stringBuilder
-    StringBuilder stringBuilder = new StringBuilder();
-
-    // iterate char
-
-    for (int rowIndex = 0; rowIndex < numRows; rowIndex++) {
-      // jumpNo = 2 * (rowNumber - i) - 2
-      boolean first = rowIndex == 0;
-      boolean last = rowIndex == numRows - 1;
-      boolean middle = !first && !last;
-      int cursor = rowIndex;
-      int cursorIndex = 0;
-      while (cursor < len) {
-        stringBuilder.append(s.charAt(cursor));
-
-        boolean odd = cursorIndex % 2 == 1;
-        boolean below = first || (middle && !odd);
-        // 2 * (rowNumber - i - 1) : 2 * i
-        int jumpNo = below ? 2 * (numRows - rowIndex - 1) : 2 * rowIndex;
-        cursor += jumpNo;
-        cursorIndex++;
-
-      }
-    }
-
-    return stringBuilder.toString();
-  }
 }
