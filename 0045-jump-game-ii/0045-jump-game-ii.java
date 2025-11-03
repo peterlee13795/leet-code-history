a01aed305af5af9d4ade13749bee9b8b99ac50d1
이가원 <lgw888@naver.com>
2025-11-04 07:40:18 +0900

    Time: 1 ms (99.7%), Space: 47.3 MB (5.22%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,15 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
-        int jumpCount = 0;
-        int currentTargetIndex = 0;
-        int nextTargetIndex = 0;
-        for (int i = 0, len = nums.length; i < len - 1; i++) {
-            nextTargetIndex = Math.max(nextTargetIndex, i + nums[i]); // jump 가능한 최대 위치
-            if (i == currentTargetIndex) { // 점프 위치에 닿으면 + 1
-                currentTargetIndex = nextTargetIndex;
-                jumpCount++;
+        int jumps = 0;
+        int end = 0;      // 현재 점프에서 도달할 수 있는 최대 위치
+        int farthest = 0; // 현재까지 탐색 중 가장 멀리 갈 수 있는 위치
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            
+            // 현재 점프의 끝에 도달했을 때
+            if (i == end) {
+                jumps++;
+                end = farthest;
             }
         }
-        return jumpCount;
+        return jumps;
     }
 }
