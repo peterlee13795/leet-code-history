999dd92ec945d61a209b71f530c63746ddbfe61b
이가원 <lgw888@naver.com>
2025-11-19 11:07:30 +0900

    Time: 4 ms (60.41%), Space: 50 MB (21.64%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,28 +1,35 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
+        // init m, n
+        int m =grid.length;
         int n = grid[0].length;
-        int[][] dp = new int[m][n];
 
+        // int[][] dp
+        int[][] dp = new int[m][n];
+        // dp[0][0] = grid[0][0]
         dp[0][0] = grid[0][0];
 
-        // 첫 행 초기화
-        for (int j = 1; j < n; j++) {
-            dp[0][j] = dp[0][j-1] + grid[0][j];
+        // init i=1
+        // dp[0][i] = dp[0][i-1] + grid[0][i]
+        for(int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i-1] + grid[0][i];
         }
 
-        // 첫 열 초기화
-        for (int i = 1; i < m; i++) {
+        // init y=1
+        // dp[y][0] = dp[y-1][0] = grid[y][0]
+        for(int i = 1; i < m; i++) {
             dp[i][0] = dp[i-1][0] + grid[i][0];
         }
 
-        // 나머지 칸
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
+        // iterate y,x
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                // dp[i][j] = grid[i][j] + math.min(dp[i-1][j], dp[i][j-1])
                 dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
             }
         }
-
+        
+        // return dp[m-1][n-1]
         return dp[m-1][n-1];
     }
 }
