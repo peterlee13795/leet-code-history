d56d36cbd32c073a0b3c55713ac3f7cc913fb431
이가원 <lgw888@naver.com>
2025-11-19 10:22:05 +0900

    Time: 0 ms (100%), Space: 43.3 MB (23.91%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,42 +1,40 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-    /**
-     * 아이디어
-     *  dp
-     *  dp[i][j] = dp[i-1][j] + dp[i][j - 1]
-     */
-    // 초기화 (길이, dp)
-    int r = obstacleGrid.length;
-    int c = obstacleGrid[0].length;
-    int[][] dp = new int[r][c];
-    if (dp[0][0] == 1) {
-      return 0;
-    }
-
-    // y축, x축 = 0
-    for (int i = 0; i < r; i++) {
-      if (obstacleGrid[i][0] == 1) {
-        break;
-      }
-      dp[i][0] = 1;
-    }
-    for (int i = 0; i < c; i++) {
-      if (obstacleGrid[0][i] == 1) {
-        break;
-      }
-      dp[0][i] = 1;
-    }
-
-    // grid 반복 & dp 입력
-    for (int i = 1; i < r; i++) {
-      for (int j = 1; j < c; j++) {
-        // dp[i][j] = dp[i-1][j] + dp[i][j-1]
-        if (obstacleGrid[i][j] == 0) {
-          dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        // 만약 시작점이 장애물이라면 경로 없음
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
         }
-      }
-    }
 
-    return dp[r - 1][c - 1];
-  }
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[] dp = new int[n];
+        dp[0] = 1;  // 시작점
+
+        // 첫 행 초기화 (i = 0)
+        for (int j = 1; j < n; j++) {
+            if (obstacleGrid[0][j] == 1) {
+                dp[j] = 0;
+            } else {
+                dp[j] = dp[j - 1];  // 왼쪽에서만 올 수 있음
+            }
+        }
+
+        // 이후 행 반복
+        for (int i = 1; i < m; i++) {
+            // 첫 열 처리 (j = 0)
+            if (obstacleGrid[i][0] == 1) {
+                dp[0] = 0;  // 이 열은 장애물이 생기면 경로 없음
+            }
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                } else {
+                    dp[j] = dp[j] + dp[j - 1];
+                }
+            }
+        }
+
+        return dp[n -1];
+
+    }
 }
