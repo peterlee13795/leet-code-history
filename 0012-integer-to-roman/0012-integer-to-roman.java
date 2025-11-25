d7fc812ec805e345f7fc406c72c5e24072c614e7
이가원 <lgw888@naver.com>
2025-11-26 00:33:13 +0900

    Time: 3 ms (99.05%), Space: 46.4 MB (12.15%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,37 +1,17 @@
 class Solution {
     public String intToRoman(int num) {
-        // StringBuilder
-        StringBuilder stringBuilder = new StringBuilder();
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
-        // pairs, 1000 -> M ..
-        Map<Integer, String> pairs = new LinkedHashMap<>();
-        pairs.put(1000, "M");
-        pairs.put(900, "CM");
-        pairs.put(500, "D");
-        pairs.put(400, "CD");
-        pairs.put(100, "C");
-        pairs.put(90, "XC");
-        pairs.put(50, "L");
-        pairs.put(40, "XL");
-        pairs.put(10, "X");
-        pairs.put(9, "IX");
-        pairs.put(5, "V");
-        pairs.put(4, "IV");
-        pairs.put(1, "I");
+        StringBuilder sb = new StringBuilder();
 
-        // iterate from 1000 to ..
-        for(Integer key: pairs.keySet()) {
-            // if ( > 0 ? ) append StringBuilder
-            int div = num / key;
-            if(div > 0) {
-                String c0 = pairs.get(key);
-                for(int i = 0 ; i < div ; i ++) {
-                    stringBuilder.append(c0);
-                }
-                num %= key;
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                sb.append(romans[i]);
             }
         }
-        // return
-        return stringBuilder.toString();
+
+        return sb.toString();
     }
 }
