8ae37dea8003a9fac9c19df282e5231931777dc4
이가원 <lgw888@naver.com>
2025-11-29 10:22:04 +0900

    Time: 148 ms (45.26%), Space: 42.9 MB (43.77%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,48 +1,58 @@
 class Solution {
-    int xlen = 0;
-    int ylen = 0;
-    int[] ydir = new int [] {-1,1,0,0};
-    int[] xdir = new int[] {0,0,-1,1};
-    int slen = 0;
+
+    boolean[][] visits;
+    char[][] board;
+    char[] chars;
+    int wlen;
+    int ylen;
+    int xlen;
+    
+    int[] ydir = {-1,1,0,0};
+    int[] xdir = {0,0,1,-1};
 
     public boolean exist(char[][] board, String word) {
-        // direction list
+        /**
+         * 전략
+         *  dfs
+         * 1. word 별 일치하는게 있는지 확인
+         * 2. 진입할 때, 방문여부 체크
+         * 3. 방문이 완료되었다면 방문여부 롤백
+         * 4. dfs 함수 호출하면서 단어 검증 => true|false
+         * 5. 만약 현재 단어가 마지막 단어라면 true
+         * 6. 4방향 이동
+         */
         this.ylen = board.length;
         this.xlen = board[0].length;
-        this.slen = word.length();
-
-        // dfs call
-
-        // iterate chars (y, x)
-        for(int yi = 0 ; yi < ylen; yi++) {
-            for(int xi = 0; xi < xlen; xi++) {
-                if(board[yi][xi] != word.charAt(0)) continue;
-                if(slen == 1) return true;
-                boolean[][] visits = new boolean[ylen][xlen];
-                visits[yi][xi] = true; // 시작점 방문 표시
-
-                if(dfs(board, visits, word, yi, xi, 1)) return true;
+        this.wlen = word.length();
+        this.visits = new boolean[this.ylen][this.xlen];
+        this.board = board;
+        this.chars = word.toCharArray();
+        
+        for(int y=0; y < this.ylen; y++) {
+            for(int x=0; x < this.xlen; x++) {
+                if(dfs(y, x, 0)) {
+                    return true;
+                }
             }
         }
-        
         return false;
     }
-
-    boolean dfs(char[][] board, boolean[][] visits, String word, int y, int x, int index) {
-        // iterate dir
-        for(int i = 0; i < 4; i++) {
+    
+    
+    boolean dfs(int y, int x, int clen) {
+        if(this.board[y][x] != this.chars[clen]) return false;
+        if(clen == this.wlen - 1) return true;
+        this.visits[y][x] = true;
+        
+        for(int i = 0 ; i < 4; i++) {
             int ny = y + this.ydir[i];
             int nx = x + this.xdir[i];
-            if(ny < 0 || ny == this.ylen) continue;
-            if(nx < 0 || nx == this.xlen) continue;                
-            if(board[ny][nx] != word.charAt(index)) continue;
-            if(visits[ny][nx]) continue;
-            visits[ny][nx] = true;
-            if(index == this.slen - 1) return true;
-            if(dfs(board, visits, word, ny, nx, index+1)) return true;
-            visits[ny][nx] = false;
+            if(ny <0 || ny >= this.ylen || nx < 0 || nx >= this.xlen || this.visits[ny][nx]) continue;
+            if(dfs(ny, nx, clen + 1)) {
+                return true;
+            }
         }
-
+        this.visits[y][x] = false;
         return false;
     }
 }
