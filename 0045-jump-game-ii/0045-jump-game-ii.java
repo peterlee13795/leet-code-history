6c8809b7d04ce70bfbc1c002b0c291c4ce75c329
이가원 <lgw888@naver.com>
2025-12-06 16:12:42 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,18 +1,5 @@
 class Solution {
     public int jump(int[] nums) {
-        int jumps = 0;
-        int end = 0;      // 현재 점프에서 도달할 수 있는 최대 위치
-        int farthest = 0; // 현재까지 탐색 중 가장 멀리 갈 수 있는 위치
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            farthest = Math.max(farthest, i + nums[i]);
-            
-            // 현재 점프의 끝에 도달했을 때
-            if (i == end) {
-                jumps++;
-                end = farthest;
-            }
-        }
-        return jumps;
+        
     }
 }
