01cc325c47b8096c54d48d980681512c4df0a958
이가원 <lgw888@naver.com>
2026-01-06 00:25:52 +0900

    Time: 0 ms (100%), Space: 45.4 MB (61.8%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-    int profit = 0;
-    for (int i = 0; i < prices.length - 1; i++) {
-      if (prices[i + 1] > prices[i]) {
-        profit += prices[i + 1] - prices[i];  // 가격이 상승하는 부분에서 매도 후 매수
-      }
-    }
-    return profit;
+        int profit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+        if (prices[i + 1] > prices[i]) {
+            profit += prices[i + 1] - prices[i];  // 가격이 상승하는 부분에서 매도 후 매수
+        }
+        }
+        return profit;
   }
 }
