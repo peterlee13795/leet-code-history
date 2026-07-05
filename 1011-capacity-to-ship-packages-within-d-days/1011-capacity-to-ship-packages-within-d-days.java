01b75dae1a653e90f53a17a61ec42257c349d01f
이가원 <lgw888@naver.com>
2026-07-05 23:44:31 +0900

    Time: 10 ms (84.81%), Space: 50.2 MB (36.84%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,37 +1,41 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int left = 0;
-        int right = 0;
+        // left, right
+        int left = 0, right = 0;
 
-        for (int weight : weights) {
+        // right = total weight of weights
+        for(int weight: weights) {
             left = Math.max(left, weight);
             right += weight;
         }
 
-        while (left < right) {
-
+        // while(left < right)
+        while(left < right) {
+            // get mid
             int mid = left + (right - left) / 2;
 
+            // requiredDays, currentWeight
             int requiredDays = 1;
             int currentWeight = 0;
 
-            for (int weight : weights) {
-
-                if (currentWeight + weight > mid) {
-                    requiredDays++;
+            // iterate weights
+            for(int weight: weights) {
+                // if(currentWeight + weight > mid) => crreuntWeight = weight, requiredDays += 1,
+                if(currentWeight + weight > mid) {
                     currentWeight = weight;
-                } else {
+                    requiredDays += 1;
+                } else { // else currentWeight += weight
                     currentWeight += weight;
                 }
             }
 
-            if (requiredDays <= days) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
+            // if (requiredDays <= days) right = mid
+            if(requiredDays <= days) right = mid;
+            // else left + 1
+            else left = mid + 1;
         }
 
+        // return left
         return left;
     }
 }
